41e85609420a04c3bf369c1fc7328d05b277b7a5
Deiner Villalobos <dvillalobos@pricesmart.com>
2025-08-25 10:22:59 -0600

    removing error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 'use client';
-'use client';
-import { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Home() {
-  useEffect(() => {
-    import('bootstrap/dist/js/bootstrap.bundle.min.js');
-  }, []);
   return (
     <div>
       <h1>Hello</h1>
